683d3e8a12af8b11d742e8983d94cbd15c8cdfcb
Piotr Ekert <piotrekert90@gmail.com>
2019-05-22 20:09:05 +0200

    Setting up `data/remote` package.

--- app/src/main/java/com/piotrek1543/example/todoapp/data/model/Task.kt
+++ app/src/main/java/com/piotrek1543/example/todoapp/data/model/Task.kt
@@ -3,7 +3,7 @@ package com.piotrek1543.example.todoapp.data.model
 import java.util.*
 
 data class Task(
-        val id: String? = UUID.randomUUID().toString(),
+        val id: String = UUID.randomUUID().toString(),
         val title: String?,
         val description: String?,
         val completed: Boolean = false
